a80226ded0581606f7111c5124eaabe71f90b779
Jerrorld <jerrorldk@gmail.com>
2023-02-07 19:10:54 -0800

    updated navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,17 +3,12 @@ import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 
 import MyDiet from "./components/MyDiet";
+import NavBar1 from "./components/NavBar1";
 
 function App() {
   return (
     <div>
-      <Navbar bg="light" style={{marginBottom: "20px"}}>
-        <Container>
-          <Navbar.Brand href="#home">
-            Nu-Tracker
-          </Navbar.Brand>
-        </Container>
-      </Navbar>
+      <NavBar1 />
       <Container>
         <MyDiet />
       </Container>
