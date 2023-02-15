7ecfdc9e5950ea055b737901cf7543635447946f
Eric <ericlg.link@gmail.com>
2023-02-14 17:05:39 -0800

    Create signup hook

---
+++ src/hooks/useSignup.js
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import axios from 'axios';
+import { useUserContext } from './useUserContext';
+
+export const useSignup = () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const { setUserData } = useUserContext();
+
+  const signup = async (username, email, password) => {
+    try {
+      setLoading(true);
+      setError(null);
+
+      const res = await axios.post('/signup', {
+        username,
+        email,
+        password,
+      });
+
+      setUserData(res.data);
+    } catch (e) {
+      setError(e.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { signup, loading, error };
+};
