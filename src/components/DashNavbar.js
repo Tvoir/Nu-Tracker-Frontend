86b81879ca384a917eb7fab3ea503f6fe3ec8e96
Jerrorld <jerrorldk@gmail.com>
2023-02-19 03:27:44 -0800

    updated navbar to have same font as the rest of the site and cleaned up some code

--- src/components/DashNavbar.js
+++ src/components/DashNavbar.js
@@ -24,12 +24,12 @@ const DashNavbar = () => {
   return (
     <Navbar collapseOnSelect expand="lg" className='navbar' style={{padding: "20px"}}>
       <Container>
-      <Navbar.Brand href="/">Nu-Tracker</Navbar.Brand>
+      <Navbar.Brand href="/" className='small-caps-font24'>Nu-Tracker</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className='me-auto'>
-            <Nav.Link href='/'>Dashboard</Nav.Link>
-            <Nav.Link href='/add-entry'>Add Entry</Nav.Link>
+            <Nav.Link href='/' className='small-caps-font24'>Dashboard</Nav.Link>
+            <Nav.Link href='/add-entry' className='small-caps-font24'>Add Entry</Nav.Link>
           </Nav>
       
       </Navbar.Collapse>
